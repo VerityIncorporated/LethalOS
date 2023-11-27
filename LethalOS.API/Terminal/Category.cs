d109e25c51590f483bec01286d8ae17f1983b956
Verity <83372423+VerityIncorporated@users.noreply.github.com>
2023-11-27 11:04:11 +0000

    Documentation for Category and Manager

--- LethalOS.API/Terminal/Category.cs
+++ LethalOS.API/Terminal/Category.cs
@@ -1,12 +1,38 @@
+using LethalOS.API;
+
 namespace LethalOS.API.Terminal;
 
+/// <summary>
+/// Represents a category in the terminal, grouping related modules.
+/// </summary>
 public class Category
 {
+    /// <summary>
+    /// Gets or sets the name of the category.
+    /// </summary>
     public string Name { get; set; }
+
+    /// <summary>
+    /// Gets or sets the description of the category.
+    /// </summary>
     public string Description { get; set; }
+
+    /// <summary>
+    /// Gets or sets the keyword used to identify the category in the terminal.
+    /// </summary>
     public string Keyword { get; set; }
+
+    /// <summary>
+    /// Gets the list of modules belonging to this category.
+    /// </summary>
     private List<ModuleBase> Modules { get; set; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="Category"/> class.
+    /// </summary>
+    /// <param name="name">The name of the category.</param>
+    /// <param name="description">The description of the category.</param>
+    /// <param name="keyword">The keyword used to identify the category.</param>
     public Category(string name, string description, string keyword)
     {
         Name = name;
@@ -14,11 +40,19 @@ public class Category
         Keyword = keyword;
         Modules = new List<ModuleBase>();
     }
-    
+
+    /// <summary>
+    /// Adds a module to this category.
+    /// </summary>
+    /// <param name="module">The module to be added.</param>
     public void AddModule(ModuleBase module)
     {
         Modules.Add(module);
     }
 
+    /// <summary>
+    /// Retrieves the list of modules belonging to this category.
+    /// </summary>
+    /// <returns>The list of modules.</returns>
     public List<ModuleBase> GetModules() => Modules;
 }
--- LethalOS.API/Terminal/Manager.cs
+++ LethalOS.API/Terminal/Manager.cs
@@ -1,14 +1,23 @@
 namespace LethalOS.API.Terminal;
 
+/// <summary>
+/// Represents the manager for menus and modules in the terminal.
+/// </summary>
 public abstract class Manager
 {
-    public static readonly List<Menu> Menus = new();
-    public static readonly List<ModuleBase> Modules = new();
+    internal static readonly List<Menu> Menus = new();
     
+    internal static readonly List<ModuleBase> Modules = new();
+
+    /// <summary>
+    /// Adds a menu to the terminal manager.
+    /// </summary>
+    /// <param name="menu">The menu to be added.</param>
     public static void AddMenu(Menu menu)
     {
         Menus.Add(menu);
-        
+
+        // Add modules from the added menu to the global modules list
         foreach (var module in menu.GetCategories.SelectMany(category => category.GetModules()))
         {
             Modules.Add(module);
